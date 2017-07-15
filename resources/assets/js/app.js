8019d74ff61550f8647f72fe62b5d2f8c151268c
carlos anselmi <carlosanselmi2@gmail.com>
2017-07-15 17:25:33 -0400

    bootstrap 4

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,7 +5,10 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+window.$ = window.jQuery = require('jquery');
+window.Tether = require('tether');
+
+require('bootstrap');
 
 window.Vue = require('vue');
 
@@ -20,3 +23,21 @@ Vue.component('example', require('./components/Example.vue'));
 const app = new Vue({
     el: '#app'
 });
+
+$(".header").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Collapse" : "Expand";
+        });
+    });
+
+});
+
